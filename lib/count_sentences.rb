aee9da1f129ec9519de53eb849c4424757e5f543
Carrie Jones <c.jessica.jones@gmail.com>
2020-09-16 22:15:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,6 +17,7 @@ class String
   def count_sentences
     int = 0
     if self.sentence? || self.question? || self.exclamation?
+      pry
       self.split(/[.?!]\s/)
       int = self.count
     end
