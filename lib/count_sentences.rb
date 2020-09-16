caab0105667f23ab1fb773c6f775677c90c095c5
Carrie Jones <c.jessica.jones@gmail.com>
2020-09-16 22:34:15 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,7 +18,7 @@ class String
     int = 0
     if self.sentence? || self.question? || self.exclamation?
       self.split('!', 4)
-      
+      pry
       int = self.count
     end
     return int
